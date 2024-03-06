0eac2d7971b8f1aafe09653af771f7f1414482f8
root <root@web01-kevin.kevin.local>
2024-03-05 19:08:53 -0500

    hardening ssh

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author Kevin
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#remoes root ability to ssh in
+echo "code goes here"
